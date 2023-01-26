b4f05f138ade875138cb6bfbe0f14d6b6088f059
DevarajP2713 <devarajupm7@gmail,com>
2023-01-26 19:41:06 +0530

    Deva Develop

--- src/webparts/feedBack/FeedBackWebPart.ts
+++ src/webparts/feedBack/FeedBackWebPart.ts
@@ -1,41 +1,56 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IReadonlyTheme } from '@microsoft/sp-component-base';
-
-import * as strings from 'FeedBackWebPartStrings';
-import FeedBack from './components/FeedBack';
-import { IFeedBackProps } from './components/IFeedBackProps';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import {
+  BaseClientSideWebPart,
+  WebPartContext,
+} from "@microsoft/sp-webpart-base";
+import { IReadonlyTheme } from "@microsoft/sp-component-base";
+import * as strings from "FeedBackWebPartStrings";
+import FeedBack from "./components/FeedBack";
+import { IFeedBackProps } from "./components/IFeedBackProps";
+import { sp } from "@pnp/sp";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/fields";
+import "@pnp/sp/items";
+import "@pnp/sp/site-users/web";
+import "@pnp/sp/attachments";
+import "@pnp/sp/site-groups/web";
 
 export interface IFeedBackWebPartProps {
   description: string;
+  context: WebPartContext;
+  spcontext: any;
 }
 
 export default class FeedBackWebPart extends BaseClientSideWebPart<IFeedBackWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
 
   protected onInit(): Promise<void> {
-    this._environmentMessage = this._getEnvironmentMessage();
-
-    return super.onInit();
+    return super.onInit().then((_) => {
+      sp.setup({
+        spfxContext: this.context,
+      });
+    });
   }
 
   public render(): void {
     const element: React.ReactElement<IFeedBackProps> = React.createElement(
       FeedBack,
       {
-        description: this.properties.description,
-        isDarkTheme: this._isDarkTheme,
-        environmentMessage: this._environmentMessage,
-        hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        // description: this.properties.description,
+        // isDarkTheme: this._isDarkTheme,
+        // environmentMessage: this._environmentMessage,
+        // hasTeamsContext: !!this.context.sdks.microsoftTeams,
+        // userDisplayName: this.context.pageContext.user.displayName
+        context: this.context,
+        spcontext: this.context,
       }
     );
 
@@ -43,11 +58,16 @@ export default class FeedBackWebPart extends BaseClientSideWebPart<IFeedBackWebP
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
@@ -56,13 +76,13 @@ export default class FeedBackWebPart extends BaseClientSideWebPart<IFeedBackWebP
     }
 
     this._isDarkTheme = !!currentTheme.isInverted;
-    const {
-      semanticColors
-    } = currentTheme;
-    this.domElement.style.setProperty('--bodyText', semanticColors.bodyText);
-    this.domElement.style.setProperty('--link', semanticColors.link);
-    this.domElement.style.setProperty('--linkHovered', semanticColors.linkHovered);
-
+    const { semanticColors } = currentTheme;
+    this.domElement.style.setProperty("--bodyText", semanticColors.bodyText);
+    this.domElement.style.setProperty("--link", semanticColors.link);
+    this.domElement.style.setProperty(
+      "--linkHovered",
+      semanticColors.linkHovered
+    );
   }
 
   protected onDispose(): void {
@@ -70,7 +90,7 @@ export default class FeedBackWebPart extends BaseClientSideWebPart<IFeedBackWebP
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -78,20 +98,20 @@ export default class FeedBackWebPart extends BaseClientSideWebPart<IFeedBackWebP
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
---
+++ src/webparts/feedBack/components/MainFeedback.tsx
@@ -0,0 +1,203 @@
+import * as React from "react";
+import styles from "./FeedBack.module.scss";
+import { CircularProgress } from "@material-ui/core";
+import { TextField, Label, ITextFieldStyles, Spinner } from "@fluentui/react";
+import { useState, useEffect } from "react";
+
+interface IFeedBack {
+  Title: string;
+  Feedback: string;
+}
+
+const MainFeedback = (props: any) => {
+  /* Variable-decluration section start */
+  let feedBackObj: IFeedBack = {
+    Title: "",
+    Feedback: "",
+  };
+
+  let errFeedback: IFeedBack = {
+    Title: "",
+    Feedback: "",
+  };
+  /* Variable-decluration section end */
+
+  /* State-decluration section start */
+  const [masterFeedBack, setMasterFeedBack] = useState<IFeedBack>(feedBackObj);
+  const [errorFeedBack, setErrorFeedBack] = useState<IFeedBack>(errFeedback);
+  const [isLoader, setIsLoader] = useState<boolean>(false);
+  const [isSpinner, setIsSpinner] = useState<boolean>(false);
+  /* State-decluration section end */
+
+  /* Style section start */
+  const longTextBoxStyles: Partial<ITextFieldStyles> = {
+    root: { width: "100%", margin: "5px 0px" },
+    fieldGroup: {
+      height: 80,
+      border: "1px solid #00584d",
+      borderRadius: "3px",
+      ":after": { border: "1px solid #00584d" },
+    },
+    field: { fontSize: 14 },
+  };
+
+  const textBoxStyles: Partial<ITextFieldStyles> = {
+    root: {
+      width: "100%",
+      margin: "5px 10px 0px 0px",
+    },
+    fieldGroup: {
+      border: "1px solid #00584d",
+      borderRadius: "3px",
+      ":after": { border: "1px solid #00584d" },
+    },
+    field: {
+      fontSize: 14,
+    },
+  };
+
+  const textBoxErrorStyles: Partial<ITextFieldStyles> = {
+    root: { width: "100%", margin: "5px 10px 0px 0px" },
+    fieldGroup: {
+      border: "2px solid #f00",
+      ":hover": { border: "2px solid #f00" },
+      ":after": { border: "2px solid #f00" },
+    },
+    field: { fontSize: 14 },
+  };
+  /* Style section end */
+
+  /* Function section start */
+  // get error function section
+  const getErrorFunction = (error: any) => {
+    console.log(error);
+  };
+  // validation function section
+  const getvalidation = () => {
+    let errValidation: IFeedBack = {
+      Title: "",
+      Feedback: "",
+    };
+    if (!masterFeedBack.Title) {
+      errValidation.Title = "Please enter your name";
+      setErrorFeedBack({ ...errValidation });
+      setIsSpinner(false);
+    } else if (!masterFeedBack.Feedback) {
+      errValidation.Feedback = "Please enter your feedback";
+      setErrorFeedBack({ ...errValidation });
+      setIsSpinner(false);
+    } else {
+      setErrorFeedBack({ ...errValidation });
+      addRecord();
+    }
+  };
+
+  // get record add function section
+  const addRecord = async () => {
+    await props.sp.web.lists
+      .getByTitle("Feedback")
+      .items.add(masterFeedBack)
+      .then((response: any) => {
+        setMasterFeedBack({
+          Title: "",
+          Feedback: "",
+        });
+        setIsSpinner(false);
+      })
+      .catch((error: any) => {
+        getErrorFunction(error);
+      });
+  };
+
+  // useEffect function section
+  useEffect(() => {
+    setIsLoader(false);
+  }, []);
+  /* Function section end */
+
+  return (
+    <>
+      {isLoader ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            <CircularProgress style={{ color: "blue" }} />
+          </div>
+        </>
+      ) : (
+        <>
+          {/* Feedback section start */}
+          <div className={styles.FeedBackBodySection}>
+            {/* Lable section */}
+            <div className={styles.Header}>Host Hub Feedback</div>
+
+            {/* Feedback Text Field section start */}
+            <div>
+              <div>
+                <Label required className={styles.LabelSection}>
+                  1.Your Name
+                </Label>
+                <TextField
+                  styles={
+                    errorFeedBack.Title ? textBoxErrorStyles : textBoxStyles
+                  }
+                  placeholder="Enter your answer"
+                  value={masterFeedBack.Title}
+                  onChange={(e: any, value: string) => {
+                    masterFeedBack.Title = value;
+                    setMasterFeedBack({ ...masterFeedBack });
+                  }}
+                />
+              </div>
+              <div>
+                <Label required className={styles.LabelSection}>
+                  2.Feedback
+                </Label>
+                <TextField
+                  styles={
+                    errorFeedBack.Feedback
+                      ? textBoxErrorStyles
+                      : longTextBoxStyles
+                  }
+                  placeholder="Enter your answer"
+                  multiline
+                  value={masterFeedBack.Feedback}
+                  onChange={(e: any, value: string) => {
+                    masterFeedBack.Feedback = value;
+                    setMasterFeedBack({ ...masterFeedBack });
+                  }}
+                />
+              </div>
+            </div>
+            {/* Feedback Text Field section end */}
+
+            {/* BTN section start */}
+            <div>
+              <div style={{ color: "red", fontWeight: "600" }}>
+                {errorFeedBack.Title ? `* ${errorFeedBack.Title}` : ""}
+                {errorFeedBack.Feedback ? `* ${errorFeedBack.Feedback}` : ""}
+              </div>
+              <button
+                disabled={isSpinner}
+                className={styles.btnSection}
+                onClick={() => (setIsSpinner(true), getvalidation())}
+              >
+                {isSpinner ? <Spinner /> : "SUBMIT"}
+              </button>
+            </div>
+            {/* BTN section end */}
+          </div>
+          {/* Feedback section end */}
+        </>
+      )}
+    </>
+  );
+};
+
+export default MainFeedback;
